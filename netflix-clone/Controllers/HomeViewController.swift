0f308c993930f23ade9fac1401e9037e731f89b4
Nguyễn Khắc Vũ <chodiem.io@gmail.com>
2022-08-11 17:21:07 +0700

    initial commit

---
+++ netflix-clone/Controllers/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  netflix-clone
+//
+//  Created by Vu Khac on 11/08/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }    
+}
---
+++ netflix-clone/Controllers/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  netflix-clone
+//
+//  Created by Vu Khac on 11/08/2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
